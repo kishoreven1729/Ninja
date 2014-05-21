7110d46830b8ce83573ea92cafe6a5d221db6378
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-21 00:13:06 -0700

    i TIRED

---
+++ Code/Ninja/Assets/Scripts/StateManager/State.cs
@@ -0,0 +1,54 @@
+﻿#region References
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+#endregion
+
+public class State
+{
+	#region Private Variables
+	private object		stateTransitionData;
+	#endregion
+
+	#region Public Variables
+	public string 		stateName;
+	public Func<int>	stateUpdateFunction;
+	public string		animationTriggerString;
+	#endregion
+
+	#region Constructor
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CharacterState"/> class. Each state is a node in the <see cref="CharacterStateManager"/>
+	/// </summary>
+	/// <param name="name">Name of the State for use in dictionary.</param>
+	/// <param name="udpateFunction">Udpate function that will called in character update loop when the state is active.</param>
+	/// <param name="animationTriggerValue">Animation trigger string if the state changes needs an animation to be played. Pass null if there isn't any.</param>
+	public State(string name, Func<int> udpateFunction, string animationTriggerValue)
+	{	
+		stateName = name;
+		stateUpdateFunction = udpateFunction;
+		animationTriggerString = animationTriggerValue;
+	}
+	#endregion
+	
+	#region Methods
+	public void PushTransitionData(object transitionData)
+	{
+		stateTransitionData = transitionData;
+	}
+
+	public object PullTransitionData()
+	{
+		if(stateTransitionData != null)
+		{
+			object returnData = stateTransitionData;
+			stateTransitionData = null;
+
+			return returnData;
+		}
+
+		return null;
+	}
+	#endregion
+}
---
+++ Code/Ninja/Assets/Scripts/StateManager/StateManager.cs
@@ -0,0 +1,113 @@
+﻿#region References
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+#endregion
+
+public class StateManager 
+{
+	#region Private Variables
+	private Animator 								_characterAnimator;
+	private Dictionary<string, State>		_stateDictionary;
+	private string									_defaultStateName;
+	#endregion
+
+	#region Public Variables
+	public State 		currentCharacterState;
+	#endregion
+
+	#region Constructor
+	public StateManager(Animator characterAnimator)
+	{
+		_characterAnimator = characterAnimator;
+
+		_stateDictionary = new Dictionary<string,State>();
+	}
+
+	/// <summary>
+	/// Sets the default state. The default state is also the starting state of the StateManager. Eg Idle. Must be called after Adding states to the StateManager
+	/// </summary>
+	/// <param name="defaultStateName">Default state name given in the dictionary.</param>
+	public void SetDefaultState(string defaultStateName)
+	{
+		currentCharacterState = _stateDictionary[defaultStateName];
+
+		_defaultStateName = defaultStateName;
+	}
+	#endregion
+
+	#region Methods
+	public void AddCharacterState(State state)
+	{
+		try
+		{
+			_stateDictionary.Add(state.stateName, state);
+		}
+		catch(System.Exception ex)
+		{
+			Debug.Log("CharacterStateManager-AddCharacterState: " + ex.Message);
+		}
+	}
+
+	public void RemoveCharacterState(string stateName)
+	{
+		try
+		{
+			_stateDictionary.Remove(stateName);
+		}
+		catch(System.Exception ex)
+		{
+			Debug.Log("CharacterStateManager-RemoveCharacterState: " + ex.Message);
+		}
+	}
+
+	public void SwitchToState(string stateName)
+	{
+		try
+		{
+			currentCharacterState = _stateDictionary[stateName];
+
+			if(currentCharacterState.animationTriggerString != null)
+			{
+				SetAnimationTrigger();
+			}
+		}
+		catch (System.Exception ex)
+		{
+			Debug.Log("CharacterStateManager-SwitchToState: " + ex.Message);
+		}
+	}
+
+	public void PushTransitionData(string stateName, object data)
+	{
+		_stateDictionary[stateName].PushTransitionData(data);
+	}
+
+	public object PullTransitionDataFromCurrentState()
+	{
+		return currentCharacterState.PullTransitionData();
+	}
+
+	public void ReturnToDefaultState()
+	{
+		if(_defaultStateName != null)
+		{
+			SwitchToState(_defaultStateName);
+		}
+	}
+
+	public void SetAnimationTrigger()
+	{
+		if(currentCharacterState != null && _characterAnimator.GetCurrentAnimatorStateInfo(0).IsName(currentCharacterState.animationTriggerString) == false)
+		{
+			_characterAnimator.SetTrigger(currentCharacterState.animationTriggerString);
+		}
+	}
+
+	public void RequestAnimationTriggerInCurrentState(string triggerValue)
+	{
+		_characterAnimator.SetTrigger(triggerValue);
+	}
+	#endregion
+}
